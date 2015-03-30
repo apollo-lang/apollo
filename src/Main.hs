a9653a6b17fd8bd9357da62d8f76e5490e05707a
Ben Kogan <benkogan@me.com>
2015-03-30 17:56:41 -0400

    Add basic Eval and simple conditional test

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,27 @@
 import Parser
+import Eval
+import Expr
+
+main :: IO ()
+main = parseExpr
+
+-- Assign `main` to `parseExpr` to evaluate a program:
+
+parseExpr :: IO ()
+parseExpr = getContents >>= putStrLn . readExpr
+
+readExpr :: String -> String
+readExpr = show . map eval . map getExpr . getStmts . parseProgram
+  where getStmts (Program stmts) = stmts
+        getExpr x = case x of
+                      (StExp expr) -> expr
+                      (StDef _)    -> error "TODO: not ready to handle Defs yet"
+
+-- Assign `main` to `parseAst` to parse a program's syntax tree:
+
+parseAst :: IO ()
+parseAst = getContents >>= putStrLn . ast
 
 ast :: String -> String
 ast = show . parseProgram
-
-main :: IO ()
-main = getContents >>= putStrLn . ast
 
