6088118432cabe1145a5d17711fa18068c8f8fbb
Ben Kogan <benkogan@me.com>
2015-03-30 18:49:50 -0400

    Add binary operation evaluation

--- src/Eval.hs
+++ src/Eval.hs
@@ -8,12 +8,34 @@ eval val@(ApolloInt      _) = val
 eval val@(ApolloBool     _) = val
 eval val@(ApolloDuration _) = val
 eval val@(ApolloPitch    _) = val
-eval (Cond tst csq alt)     = if checkBool $ eval tst
+eval (Binary binExp)        = evalBinOp binExp
+eval (Cond tst csq alt)     = if getBool $ eval tst
                               then eval csq
                               else eval alt
 eval _ = error "evaluation error"
 
-checkBool :: Expr -> Bool
-checkBool (ApolloBool bool) = bool
-checkBool x                 = error $ "expected Bool; instead: " ++ show x
+evalBinOp :: BinOp -> Expr
+evalBinOp (Add e1 e2) = ApolloInt  $ (+)  (getInt  e1) (getInt  e2)
+evalBinOp (Sub e1 e2) = ApolloInt  $ (-)  (getInt  e1) (getInt  e2)
+evalBinOp (Mul e1 e2) = ApolloInt  $ (*)  (getInt  e1) (getInt  e2)
+evalBinOp (Div e1 e2) = ApolloInt  $ div  (getInt  e1) (getInt  e2)
+evalBinOp (Mod e1 e2) = ApolloInt  $ mod  (getInt  e1) (getInt  e2)
+evalBinOp (Eq  e1 e2) = ApolloBool $ (==) (getBool e1) (getBool e2)
+evalBinOp (NEq e1 e2) = ApolloBool $ (/=) (getBool e1) (getBool e2)
+evalBinOp (Le  e1 e2) = ApolloBool $ (<)  (getBool e1) (getBool e2)
+evalBinOp (Gr  e1 e2) = ApolloBool $ (>)  (getBool e1) (getBool e2)
+evalBinOp (LEq e1 e2) = ApolloBool $ (<=) (getBool e1) (getBool e2)
+evalBinOp (GEq e1 e2) = ApolloBool $ (>=) (getBool e1) (getBool e2)
+evalBinOp (And e1 e2) = ApolloBool $ (&&) (getBool e1) (getBool e2)
+evalBinOp (Or  e1 e2) = ApolloBool $ (||) (getBool e1) (getBool e2)
+
+getInt :: Expr -> Int
+getInt x = case (eval x) of
+             (ApolloInt i) -> i
+             notInt        -> error $ "expected ApolloInt; instead got " ++ show notInt
+
+getBool :: Expr -> Bool
+getBool x = case (eval x) of
+              (ApolloBool b) -> b
+              notInt         -> error $ "expected ApolloBool; instead got " ++ show notInt
 
