871464bfa8d0d88ddce99852735661703f7bf159
Ben Kogan <benkogan@me.com>
2015-04-23 12:29:45 -0400

    Rename Type constructors, copy necessary fns from Types.hs
    Disambiguated type constructor names from value constructors and monad
    transformers (which usually end with "T").
    
    Created Util.hs to hold functions used for coersion etc. during parsing.
    These functions are copies of their original declarations in
    midi/Types.hs. This may ultimately prove to be a bad idea.

--- src/Expr.hs
+++ src/Expr.hs
@@ -1,42 +1,44 @@
 module Expr where
-import Types
 
 data Param
-    = Param Id Type
-    deriving Show
+  = Param Id Type
+  deriving Show
 
 type Id = String
 
 data Type
-    = Data String
-    | ListT String
-    | Function [Param] Type
-    deriving Show
+  = TData String
+  | TList String
+  | TFunc [Param] Type
+  deriving Show
 
 data Expr
-    = VInt Int
-    | VBool Bool
-    | VDuration Duration
-    | VPitch Pitch
-    | VNote Pitch Duration
-    | VChord [Pitch] Duration
-    | VList [Expr]
-    | Def Id Type Expr
-    | Name Id
-    | Block [Expr] Expr
-    | If Expr Expr Expr
-    | FnCall Id [Expr]
-    | Neg Expr
-    | Not Expr
-    | IntOp IOpr Expr Expr
-    | BoolOp BOpr Expr Expr
-    deriving Show
+  = VInt Int
+  | VBool Bool
+  | VDuration Duration
+  | VPitch Pitch
+  | VNote Pitch Duration
+  | VChord [Pitch] Duration
+  | VList [Expr]
+  | Def Id Type Expr
+  | Name Id
+  | Block [Expr] Expr
+  | If Expr Expr Expr
+  | FnCall Id [Expr]
+  | Neg Expr
+  | Not Expr
+  | IntOp IOpr Expr Expr
+  | BoolOp BOpr Expr Expr
+  deriving Show
 
 data IOpr = Add | Sub | Mul | Div | Mod
   deriving Show
 
 data BOpr = Eq | NEq | Le | Gr | LEq | GEq | And | Or
-    deriving Show
+  deriving Show
+
+data Pitch = Pitch Int deriving Show
+data Duration = Duration Int deriving Show
 
 showVal :: Expr -> String
 showVal (VInt  i) = show i
@@ -50,32 +52,4 @@ typeOf (VInt _)  = "Integer"
 typeOf (VBool _) = "Boolean"
 typeOf (VList _) = "List"
 typeOf todoVal        = "TODO: `typeOf` for " ++ show todoVal
-
-define :: Id -> Type -> Expr -> Expr
--- Coerce Int to Pitch
-define i (Data "Pitch") (VInt p)
-    | p < 0 || p > 127  = error "Pitch out of range [0, 127]"
-    | otherwise = Def i (Data "Pitch") (VPitch (Pitch p))
--- Coerce Int to Duration
-define i (Data "Duration") (VInt d)
-    | d < 1 || d > 256  = error "Duration out of range [1, 256]"
-    | otherwise = Def i (Data "Duration") (VDuration (Duration d))
--- Otherwise
-define i t e           = Def i t e
-
-unpackList :: Expr -> [Expr]
-unpackList (VList exprs) = exprs
-unpackList _ = error "Syntax error"
-
-unpackPitch :: Expr -> Pitch
-unpackPitch (VPitch p) = p
-unpackPitch _ = error "Syntax error"
-
-construct :: Type -> [Expr] -> Expr
-construct (Data "Pitch") [VInt p] = VPitch (Pitch p)
-construct (Data "Note") [VPitch p, VDuration d]
-    = VNote p d
-construct (Data "Chord") [pitches, VDuration dur]
-    = VChord (map unpackPitch (unpackList pitches)) dur
-construct _ _ = error "Syntax error"
 
