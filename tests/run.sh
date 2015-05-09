0348756bf663ef689706d215e4cf4f93d69454e9
rja2139 <rja2139@columbia.edu>
2015-05-09 15:03:11 -0400

    Blank lines are now ignored. Script commenting

--- tests/run.sh
+++ tests/run.sh
@@ -6,6 +6,7 @@ quiet=0
 exit_status=0
 compile_error=0
 
+# Several functions to print strings with colors
 green() {
   printf "${indent}\033[0;32m%s\033[0m %s\n" "$1" "$2"
 }
@@ -24,6 +25,7 @@ error () {
   } >&2
 }
 
+# Simple help interface
 usage() {
   echo
   echo "${indent}Description: run all integration tests"
@@ -37,17 +39,20 @@ usage() {
   echo
 }
 
+# Eval for normal AP files: redirects the file into apollo
 evaluate() {
   # Stop execution exceeding 10 seconds to prevent infinite loops
   ulimit -t 10
 
   ../apollo $2 < "$1" 2> /dev/null
 
+  # if Apollo exits with 1 something severe happened!
   if test $? -eq 1; then
     echo "<compilation error>"
   fi
 }
 
+# Eval for APS: it echos the line to be tested and pipes it into apollo
 evalAPS() {
   # Stop execution exceeding 10 seconds to prevent infinite loops
   ulimit -t 10
@@ -59,7 +64,7 @@ evalAPS() {
   fi
 }
 
-
+# Function that runs diff and compares the apollo result against the expected result
 compare() {
   diff -Bw <(echo "${1}") <(echo "${2}")
 }
@@ -70,6 +75,11 @@ printr() {
   done <<< "$1"
 }
 
+# The main function that deals with checking files and treating them according to their extensions:
+# .ap: normal Apollo source file
+# .aps: source file where each input line must be tested separately
+# .ans: normal answer file (file containing the expected answer)
+# .ast: file containing a program's AST instead of the normal answer
 check() {
   if test "$os" == "Darwin"; then
     local pass="✔︎"
@@ -88,9 +98,11 @@ check() {
     local answ=${test/.aps/.ans}
     local name=${1/.aps/}
     if test ! -e "$answ"; then
+      # If in .ast mode (the answer file has an extension .ast) we compare the source against its AST
       local answ=${test/.aps/.ast}
       local ast=1
     fi
+    # Flag sets .aps mode
     local aps=1
   else
     local answ=${test/.ap/.ans}
@@ -113,9 +125,16 @@ check() {
 
 
   if test $aps -eq 1; then
+    # In .aps mode, each line is read and interpreted individually.
     local lineno=0
     while read -r line; do
+      if test -z "$line"; then
+        # If a line in the test file is empty, it is ignored (as well as the respective line in the ans file)
+        let lineno=lineno+1
+        continue
+      fi
       let lineno=lineno+1
+
       if test "$ast" -eq 1; then
         local interp=$(evalAPS "$line" --ast)
       else
@@ -125,10 +144,12 @@ check() {
       local answer=$(awk "NR==$lineno" "$answ")
       local result=$(compare "$interp" "$answer")
       if test -n "$result"; then
+      # If one of the lines generate an error, that test stops and no further lines are tested
         break
       fi
     done < "$test"
   else
+    # Much simples testing when we deal with the whole source file
     if test "$ast" -eq 1; then
       local interp=$(evaluate $test --ast)
     else
@@ -144,6 +165,7 @@ check() {
   if test -n "$result"; then
     red "$fail $name" "($test $answ)"
     if test $quiet -eq 0; then
+    # If the script is run with -q (quiet) the error message is not printed
       red
       if test $aps -eq 1; then
         red "  Error in line: $lineno"
@@ -189,9 +211,9 @@ change_dir_if_necessary() {
   fi
 }
 
+# Function that lists files with appropriate extensions in tests directory and send them to be processed
 run_tests() {
   local tests=$(ls *.ap *.aps)
-
   echo
 
   for test in $tests; do
