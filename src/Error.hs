d83c199f259a8a91ae1f114eeac86a6b0191673f
Ben Kogan <benkogan@me.com>
2015-05-10 17:45:41 -0400

    Remove todo

--- src/Error.hs
+++ src/Error.hs
@@ -49,5 +49,5 @@ trapError action = catchError action (return . show)
 
 extractValue :: Either ApolloError a -> a
 extractValue (Right val) = val
-extractValue (Left todo) = error $ "bug: extractValue called with Left (" ++ show todo ++ ")"
+extractValue (Left _)    = error $ "bug: extractValue called with Left"
 
