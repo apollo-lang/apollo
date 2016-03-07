050491d0c98c152a4f96745fccff55a0ff70e6d8
Ben Kogan <bkogan@appnexus.com>
2016-03-07 18:10:59 -0500

    Remove trailing whitespace

--- src/Version.hs
+++ src/Version.hs
@@ -2,7 +2,7 @@ module Version (
   version
 ) where
 
-import qualified Paths_apollo as P (version) 
+import qualified Paths_apollo as P (version)
 import Data.Version (showVersion)
 
 version = showVersion P.version
